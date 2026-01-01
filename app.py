c967eef0141c97940d766b7184680721c02a0b49
JAYDISH KENNEDY J <jaydishkennedy@gmail.com>
2026-01-01 23:31:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="INSECTIFICA",
+    page_icon="🐞",
+    layout="centered"
+)
+
+st.title("🐞 INSECTIFICA 🔍")
+st.subheader("AI-Powered Insect & Pest Identification")
+
+st.markdown("""
+Welcome to **INSECTIFICA**.
+
+👉 Use the **sidebar** to navigate:
+- Home  
+- About  
+- Workflow  
+- Features  
+- Contact
+""")
+
+st.success("Select a page from the sidebar 👈")
